6f9ed07f585fc95b38e89fd24be701aafaf97e88
redazures <jwu_nyc@yahoo.com>
2020-10-08 13:05:45 -0400

    new screen

--- app/screens/AccountScreen.js
+++ app/screens/AccountScreen.js
@@ -1,18 +1,56 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
-import Screen from '../components/Screen'
-import ListItems from '../components/ListItems'
+import React from "react";
+import { StyleSheet, Text, View, FlatList } from "react-native";
+
+import Screen from "../components/Screen";
+import ListItems from "../components/ListItems";
+import colors from "../config/colors";
+import Icon from '../components/Icon'
+
+const menuItems = [
+  {
+    title: "My Listings",
+    icon: {
+      name: "format-list-bulleted",
+      backgroundColor: colors.primary,
+    },
+    title: "My Messages",
+    icon: {
+      name: "email",
+      backgroundColor: colors.secondary,
+    },
+  },
+];
 
 export default function AccountScreen() {
-    return (
-        <Screen>
-            <ListItems
-                title="Jimbo Chan"
-                subTitle="Meko"
-                image={require('../assets/doggie.png')}
-            />
-        </Screen>
-    )
+  return (
+    <Screen>
+      <View style={styles.container}>
+        <ListItems
+          title="Jimbo Chan"
+          subTitle="Meko"
+          image={require("../assets/doggie.png")}
+        />
+      </View>
+      <View style={styles.container}>
+      <FlatList
+        data={menuItems}
+        keyExtractor={item => item.title}
+        renderItem={({ item }) => (
+          <ListItems
+            title={item.title} 
+            ImageComponent={
+                <Icon name={item.icon.name} backgroundColor={item.icon.backgroundColor}/>
+            }
+          />
+        )}
+      />
+      </View>
+    </Screen>
+  );
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    marginVertical: 20,
+  },
+});
