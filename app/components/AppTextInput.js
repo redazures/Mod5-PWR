018918eb70924da1ddfaefd2d50a7c6e21d628a3
redazures <jwu_nyc@yahoo.com>
2020-10-08 16:34:45 -0400

    first form

--- app/components/AppTextInput.js
+++ app/components/AppTextInput.js
@@ -7,8 +7,8 @@ import colors from '../config/colors'
 export default function AppTextInput({icon,...otherProps}) {
     return (
         <View style={styles.container}>
-            {icon && <MaterialCommunityIcons name={icon}/>}
-            <TextInput style={styles.textInput} {...otherProps} size={20} color={colors.medium}/>
+            {icon && <MaterialCommunityIcons name={icon} size={20} color={colors.medium} style={styles.icon}/>}
+            <TextInput style={styles.textInput} {...otherProps} />
         </View>
     )
 }
@@ -22,8 +22,12 @@ const styles = StyleSheet.create({
         padding:15,
         marginVertical:10,
     },
+    icon:{
+        marginRight:10
+    },
     textInput:{
         fontSize:18,
         fontFamily: Platform.OS==="android" ? "Roboto" : "Avenir",
+        color:colors.dark
     }
 })
