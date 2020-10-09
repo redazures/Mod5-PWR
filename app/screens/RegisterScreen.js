b868f4bc71e17be3d7b9212e68286d3ca6081328
redazures <jwu_nyc@yahoo.com>
2020-10-09 14:02:25 -0400

    registery

--- app/screens/RegisterScreen.js
+++ app/screens/RegisterScreen.js
@@ -2,14 +2,62 @@ import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 import * as Yup from 'yup'
 
-import {AppFormField} from '../components/forms'
+import Screen from '../components/Screen'
+import {AppFormField, SubmitButton, AppForm} from '../components/forms'
+
+const validationSchema=Yup.object().shape({
+    email:Yup.string().required().email().label("Email"),
+    name:Yup.string().required().label("Name"),
+    password:Yup.string().required().min(4).label("password"),
+})
 
 export default function RegisterScreen() {
     return (
-        <View>
-            <Text></Text>
-        </View>
+        <Screen style={styles.container}>
+            <AppForm
+                initialValues={{name:"",email:"",password:""}}
+                onSubmit={(values)=>console.log(values)}
+                validationSchema={validationSchema}
+            >
+                <AppFormField
+                    autoCorrect={false}
+                    icon='account'
+                    name='name'
+                    placeholder="Name"
+                />
+                <AppFormField
+                    autoCapitalize="none" // this prevents IOS and android to auto capitalize first letter
+                    autoCorrect={false}
+                    icon="email"
+                    keyboardType='email-address'
+                    name='email'
+                    placeholder="Email"
+                    textContentType="emailAddress" //allows IOS to autofill from keychain
+                />
+                <AppFormField
+                    autoCapitalize="none"
+                    autoCorrect={false}
+                    icon='lock'
+                    name='password'
+                    placeholder='Password'
+                    secureTextEntry
+                    textContentType='password' //allows IOS to autofill from keychain
+                />
+                <SubmitButton title={'login'}/>
+            </AppForm>
+        </Screen>
     )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container:{
+        padding:10,
+    },
+    logo:{
+        width:100,
+        height:100,
+        alignSelf:'center',
+        marginTop:50,
+        marginBottom:20,
+    }
+})
