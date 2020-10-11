d065e8a84e0eb758293c9d3490de73a56cfba347
redazures <jwu_nyc@yahoo.com>
2020-10-10 20:41:37 -0400

    updated with semi nav

--- App.js
+++ App.js
@@ -22,15 +22,66 @@ import AppPicker from "./app/components/Picker";
 import LoginScreen from './app/screens/LoginScreen'
 import RegisterScreen from './app/screens/RegisterScreen'
 import ListEditScreen from './app/screens/ListEditScreen'
-import * as ImagePicker from 'expo-image-picker'
+// import * as ImagePicker from 'expo-image-picker'
 import  * as Permission from 'expo-permissions'
 import ImageInput from "./app/components/ImageInput";
 import ImageInputList from "./app/components/ImageInputList";
+import { createStackNavigator } from '@react-navigation/stack'
+import { NavigationContainer, useNavigation } from '@react-navigation/native'
 
 export default function App() {
 
+  const Link=()=>{
+    const navigation = useNavigation()
+
+    return(
+      <Button
+        title='click'
+        onPress={()=>{
+          console.log("inside link")
+          navigation.navigate('TweetDetails')
+        }}
+      />
+    )
+
+  }
+
+  const Tweets =({navigation})=>(
+    <Screen>
+        <Text>Tweet Main Page Hola</Text>
+        <Button
+        title='click'
+        onPress={()=>{
+          console.log("inside tweets")
+          navigation.navigate('TweetDetails',{id:1})
+        }}
+        />
+    </Screen>
+  )
+//can pass entire objects in the 2 paramenter of navigate
+
+  const TweetDetails =({route})=>(
+    <Screen>
+        <Text>Tweet Details to here. I got lots to say!{route.params.id}</Text>
+    </Screen>
+  )
+
+  const Stack = createStackNavigator();
+  const StackNavigator = ()=>(
+      <Stack.Navigator 
+        screenOptions={{
+          headerStyle:{backgroundColor:'red'},
+          headerTintColor:'white'
+          }}>
+        <Stack.Screen name='Tweets' component={Tweets} />
+        <Stack.Screen name='TweetDetails' component={TweetDetails}/>
+      </Stack.Navigator>
+  )
+    
   console.log("render")
   return (
-    <ListEditScreen/>
+    <NavigationContainer>
+    <StackNavigator/>
+    </NavigationContainer>
   );
 }
