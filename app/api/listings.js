4b63d4a5be01c29501335599b36e1edd73d63c5e
redazures <jwu_nyc@yahoo.com>
2020-10-13 01:57:31 -0400

    mid

--- app/api/listings.js
+++ app/api/listings.js
@@ -7,21 +7,11 @@ const getListings = () => client.get(endpoint)
 
 const addListings = (listing, onUploadProgress) =>{
     const data = new FormData()
-    data.append('title',listing.title)
-    data.append('price',listing.price)
-    
-    data.append('categoryId',listing.category.value)
+    data.append('name',listing.name)
+    data.append('room',listing.room)
     data.append('description',listing.description)
-    listing.images.forEach((image,index)=>data.append('images',{
-        name: 'image'+index,
-        type: 'image/jpeg',
-        uri: image,
-    }))
 
-    // console.log("this is my client", client.post(endpoint))
-
-    if(listing.location)
-        data.append('location', JSON.stringify(listing.location))
+    console.log("this is my client", client.post(endpoint))
     
     return client.post(endpoint, data, {
         onUploadProgress:(progress)=>
--- app/screens/ListEditScreen.js
+++ app/screens/ListEditScreen.js
@@ -1,37 +1,26 @@
 import React, { useEffect, useState } from "react";
 import { StyleSheet } from "react-native";
 import * as Yup from "yup";
-import CategoryPickerItem from "../components/CategoryPickerItem";
 
 import { AppForm as Form, AppFormField as FormField, AppFormPicker as Picker, SubmitButton } from "../components/forms";
-import FormImagePicker from "../components/forms/FormImagePicker";
 import Screen from "../components/Screen";
 import listingsApi from '../api/listings'
-import useLocation from "../components/hooks/useLocation";
 import routes from "../components/navigation/routes";
 
 
 const validationSchema = Yup.object().shape({
-    title: Yup.string().required().min(1).label("Title"),
-    price: Yup.number().required().min(1).max(10000).label("Price"),
+    name: Yup.string().required().min(1).label("Name"),
+    room: Yup.number().required().min(1).max(999).label("Room"),
     description: Yup.string().label("Description"),
-    category: Yup.object().required().nullable().label("Category"),
-    images:Yup.array().min(1,"Please select at least at one image")      //.label("Images")//Images field is required
 });
 
-const categories = [
-    {label:"Medical", value:1, backgroundColor:'red', icon:'apps'},
-    {label:"Surgery", value:2, backgroundColor:'green', icon:'email'},
-    {label:"ICU", value:3, backgroundColor:'blue', icon:'lock'},
-];
-
 export default function ListingEditScreen({navigation}) {
-    const location = useLocation()
+    //const location = useLocation()
 
     const handleSubmit = async (listings, {resetForm}) =>{
-        // console.log(listing)
+        console.log(listings)
         const result = await listingsApi.addListings(
-            {...listings, location},
+            {...listings},
             (progress)=>console.log(progress)
         )
         // console.log(result)
@@ -44,27 +33,17 @@ export default function ListingEditScreen({navigation}) {
     return (
         <Screen style={styles.container}>
             <Form
-                initialValues={{ title: "", price: "", description: "", category: "", images:[]}}
+                initialValues={{ name: "", room: "", description: ""}}
                 onSubmit={handleSubmit}
                 validationSchema={validationSchema}
             >
-            <FormImagePicker name='images'/>
-            <FormField maxLength={255} name="title" placeholder="Title" />
+            <FormField maxLength={255} name="name" placeholder="Name" />
             <FormField
                 keyboardType="numeric"
                 maxLength={8}
-                name="price"
-                placeholder="Price"
-                //width={120}
+                name="room"
+                placeholder="Room"
             />
-            <Picker 
-                items={categories} 
-                name="category"
-                numberOfColumns={3}
-                PickerItemComponent={CategoryPickerItem}
-                placeholder="Category" 
-            /> 
-            {/* //width='50%' belongs in the picker above to change the spacing*/}
             <FormField
                 maxLength={255}
                 multiline
